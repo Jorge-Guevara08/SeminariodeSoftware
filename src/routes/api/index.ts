4455453ccf186ba8243f70d1de0c6b47fab3bad5
Jorge-Guevara08 <77889689+Jorge-Guevara08@users.noreply.github.com>
2023-05-17 18:49:02 -0600

    agregar ruta teams a index de api

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -3,10 +3,12 @@ const router = express.Router();
 
 import projectRouter from './projects';
 import securityRouter from './security';
-
+import teamsRouter from './teams';
 
 router.use('/projects', projectRouter);
 router.use('/security', securityRouter);
+router.use('/teams', teamsRouter);
+
 
 
 export default router;
