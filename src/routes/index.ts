da21f89723bd52595d8d7dd01d8289d464d9203f
Jorge-Guevara08 <77889689+Jorge-Guevara08@users.noreply.github.com>
2023-05-17 18:27:47 -0600

    Agregar nueva ruta

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,4 +5,10 @@ router.get('/', (_req, res) => {
   res.json({msg:'Hello World!'});
  });
 
+ router.get('/test', (_req, res) => {  //el primer objeto hace la solicitud y el segundo permite configurar, definir y agregar valor a lo que pide el cliente
+  res.json({msg:'Hello Test!'});
+ });
+
+
+
 export default router;
