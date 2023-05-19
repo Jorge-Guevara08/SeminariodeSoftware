f25bc8008aa8c96631f948a482402223ab8eba56
Jorge-Guevara08 <77889689+Jorge-Guevara08@users.noreply.github.com>
2023-05-18 19:14:10 -0600

    correcion echo2

--- src/routes/api/projects/index.ts
+++ src/routes/api/projects/index.ts
@@ -13,7 +13,7 @@ router.get('/echo/:msg', (req, res) => {
     res.json({msg, variable1, variable2});
 });
 
-router.post('/echo2/:msg', (req, res) => {
+router.post('/echo2/', (req, res) => {
     const {variable1='Hola', variable2='Mundo'} = req.body;
     res.json({variable1, variable2});
 });
