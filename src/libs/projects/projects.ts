a1075594d2a931f36088869c194a280a71233c48
Jorge-Guevara08 <77889689+Jorge-Guevara08@users.noreply.github.com>
2023-05-23 19:37:14 -0600

    update y delete en projects

--- src/libs/projects/projects.ts
+++ src/libs/projects/projects.ts
@@ -1,4 +1,4 @@
-export interface IProject{
+export interface IProject {
     _id?: string;
     name: string;
     description: string;
@@ -17,7 +17,7 @@ const memoryProjects: IProject[] = [];
 let createdProjects: number = 0;
 
 export const createProject = async (project: IProject) => {
-    const newProject = {...project};
+    const newProject = { ...project };
     newProject._id = (++createdProjects).toString();
     newProject.createdAt = new Date();
     newProject.updatedAt = newProject.createdAt;
@@ -25,6 +25,32 @@ export const createProject = async (project: IProject) => {
     return newProject;
 }
 
-export const getProject = async () =>{
+export const getProject = async () => {
     return memoryProjects;
+}
+
+export const updateProject = (id: string, project: Partial) => {
+    /* Obtenemos el indice del proyecto basados en el id,
+  si obtenemos un -1 es que no encontró ninguna coincidencia
+  el méotodo findIndex utiliza una función donde se compara un parametro con el
+  valor del objecto que se esta iterando, si se evalua la comparación en verdadero,
+  se asigna el indice del objeto iterado.
+  */
+    const index = memoryProjects.findIndex(p => p._id === id);
+    if (index === -1) throw new Error('Project not found');
+    // Usamos <<< spread operators ...variable >>> para extraer todas las llaves de un objeto con sus valores
+    // en este caso se destructura el projecto en el indice, el valor pasado desde el que
+    // llama la función y un atributo para actualizar la fecha de actualización,
+    // los atributos similares van sobreescribiendo los atributos ya asignados por tanto
+    // los atributos de memoryProjects[index] son sobreescritos con los atributos de project
+    // y el atributo updatedAt se sobreescribe con el nuevo Date.
+    memoryProjects[index] = { ...memoryProjects[index], ...project, updatedAt: new Date() };
+    return memoryProjects[index];
+}
+
+export const deleteProject = (id: string) => {
+    const index = memoryProjects.findIndex(p => p._id === id);
+    if (index === -1) throw new Error('Project not found');
+    memoryProjects.splice(index, 1);
+    return true;
 }
--- src/routes/api/projects/index.ts
+++ src/routes/api/projects/index.ts
@@ -1,23 +1,23 @@
 import express from 'express';   // estos es estandar
 const router = express.Router();
 
-import {createProject, getProject } from '@libs/projects/projects';
+import { createProject, getProject, updateProject, deleteProject } from '@libs/projects/projects';
 
 router.get('/', (_req, res) => {
-    res.json({version:1, scope:'projects'});
+    res.json({ version: 1, scope: 'projects' });
 });
 
 // /appi/projects/echo/hola?variable1=a&variable2=b
 
 router.get('/echo/:msg', (req, res) => {
-    const {msg} = req.params;
-    const {variable1='Hola', variable2='Mundo'} = req.query;
-    res.json({msg, variable1, variable2});
+    const { msg } = req.params;
+    const { variable1 = 'Hola', variable2 = 'Mundo' } = req.query;
+    res.json({ msg, variable1, variable2 });
 });
 
 router.post('/echo2/', (req, res) => {
-    const {variable1='Hola', variable2='Mundo'} = req.body;
-    res.json({variable1, variable2});
+    const { variable1 = 'Hola', variable2 = 'Mundo' } = req.body;
+    res.json({ variable1, variable2 });
 });
 
 router.get('/all', async (_req, res) => {
@@ -26,10 +26,24 @@ router.get('/all', async (_req, res) => {
 });
 
 router.post('/new', async (req, res) => {
-    const {name = '', description = '', isActive = false} = req.body;
-    const newProject = {name, description, isActive: (isActive && true)};
+    const { name = '', description = '', isActive = false } = req.body;
+    const newProject = { name, description, isActive: (isActive && true) };
     const createdProject = await createProject(newProject);
     res.json(createdProject);
 });
+
+router.put('/upd/:id', (req, res) => {
+    const projectId = req.params.id;
+    const { name = '', description = ''} = req.body;
+    const updatedProject = updateProject(projectId, { name, description});
+    res.json(updatedProject);
+  });
+
+
+  router.delete('/del/:id', (req, res) => {
+    const projectId = req.params.id;
+    const result = deleteProject(projectId);
+    res.json({ result});
+  })
 
 export default router; // y esto tambien
