76be118557da35619a223bbfd2508d4c96f1d40c
Jorge-Guevara08 <77889689+Jorge-Guevara08@users.noreply.github.com>
2023-05-29 18:57:32 -0600

    Creacion de clase abstracta

---
+++ src/dao/models/ProjectsDao.ts
@@ -0,0 +1,70 @@
+import { IProject } from "@libs/projects/projects";
+import { DaoBase } from "./DaoBase";
+import * as File from "fs";
+
+export class ProjectDao extends DaoBase<IProject>{
+
+    private memoryProjects: IProject[] = [];
+    private createdProjects: number = 0;
+
+    public create(item: IProject): Promise<IProject> {
+
+        const newProject = { ...item };
+        newProject._id = (++this.createdProjects).toString();
+        newProject.createdAt = new Date();
+        newProject.updatedAt = newProject.createdAt;
+        this.memoryProjects.push(newProject);
+        this.serialize();
+        return Promise.resolve(newProject);
+    }
+    public update(id: string, item: Partial<IProject>): Promise<IProject> {
+
+        const index = this.memoryProjects.findIndex(p => p._id === id);
+        if (index === -1) throw new Error('Project not found');
+        this.memoryProjects[index] = { ...this.memoryProjects[index], ...item, updatedAt: new Date() };
+        this.serialize();
+        return Promise.resolve(this.memoryProjects[index]);
+    }
+    public delete(id: string): Promise<boolean> {
+
+        const index = this.memoryProjects.findIndex(p => p._id === id);
+        if (index === -1) throw new Error('Project not found');
+        this.memoryProjects.splice(index, 1);
+        this.serialize();
+        return Promise.resolve(true);
+    }
+    public find(_item: Partial<IProject>): Promise<IProject[]> {
+
+        return Promise.resolve(this.memoryProjects);
+    }
+    public findOne(id: string): Promise<IProject> {
+
+        const project = this.memoryProjects.find(p => p._id === id);
+        if (!project) throw new Error('Project not found');
+        return Promise.resolve(project);
+    }
+
+    private serialize(): void {
+        const data = JSON.stringify({ memoryProjects: this.memoryProjects, createdProjects: this.createdProjects });
+        File.writeFileSync('project.json', data);
+    }
+
+    private deserialize(): void {
+
+        try {
+            const data = File.readFileSync('project.json', 'utf8');
+            const { memoryProjects, createdProjects } = JSON.parse(data);
+            this.memoryProjects = memoryProjects;
+            this.createdProjects = createdProjects;
+        } catch (error) {
+            console.log('Error', error);
+        }
+
+    }
+
+    constructor() {
+        super();
+        this.deserialize();
+    }
+
+}
--- src/libs/projects/projects.ts
+++ src/libs/projects/projects.ts
@@ -1,3 +1,5 @@
+import {ProjectDao} from '@dao/models/ProjectsDao';
+
 export interface IProject {
     _id?: string;
     name: string;
@@ -13,39 +15,56 @@ export interface IProject {
 //     isActive: false
 // };
 
-const memoryProjects: IProject[] = [];
-let createdProjects: number = 0;
+
+// const memoryProjects: IProject[] = [];
+// let createdProjects: number = 0;
+
+const ProjectDaoInstance = new ProjectDao();
 
 export const createProject = async (project: IProject) => {
-    const newProject = { ...project };
-    newProject._id = (++createdProjects).toString();
-    newProject.createdAt = new Date();
-    newProject.updatedAt = newProject.createdAt;
-    memoryProjects.push(newProject);
-    return newProject;
+    
+    // const newProject = { ...project };
+    // newProject._id = (++createdProjects).toString();
+    // newProject.createdAt = new Date();
+    // newProject.updatedAt = newProject.createdAt;
+    // memoryProjects.push(newProject);
+    // return newProject;
+
+    return ProjectDaoInstance.create(project);
 };
 
-export const getProjects = async () => {
-    return memoryProjects;
+export const getProjects = () => {
+    return ProjectDaoInstance.find({});
+
+    // return memoryProjects;
 };
 
-export const getProject = async (id: string) => {
-    const project = memoryProjects.find(p => p._id === id);
-    if (!project) throw new Error('Project not found');
-    return project;
+export const getProject = (id: string) => {
+
+    return ProjectDaoInstance.findOne(id);
+
+    // const project = memoryProjects.find(p => p._id === id);
+    // if (!project) throw new Error('Project not found');
+    // return project;
 }
 
 
 export const updateProject = (id: string, project: Partial<IProject>) => {
-    const index = memoryProjects.findIndex(p => p._id === id);
-    if (index === -1) throw new Error('Project not found');
-    memoryProjects[index] = { ...memoryProjects[index], ...project, updatedAt: new Date() };
-    return memoryProjects[index];
+
+    return ProjectDaoInstance.update(id, project);
+
+    // const index = memoryProjects.findIndex(p => p._id === id);
+    // if (index === -1) throw new Error('Project not found');
+    // memoryProjects[index] = { ...memoryProjects[index], ...project, updatedAt: new Date() };
+    // return memoryProjects[index];
 };
 
 export const deleteProject = (id: string) => {
-    const index = memoryProjects.findIndex(p => p._id === id);
-    if (index === -1) throw new Error('Project not found');
-    memoryProjects.splice(index, 1);
-    return true;
+
+    return ProjectDaoInstance.delete(id);
+
+    // const index = memoryProjects.findIndex(p => p._id === id);
+    // if (index === -1) throw new Error('Project not found');
+    // memoryProjects.splice(index, 1);
+    // return true;
 }
